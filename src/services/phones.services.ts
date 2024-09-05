151c3e9ff0d736e92914a405ae634d7925d34b41
Carolina do Amaral <carolinafrederico32@hotmail.com>
2024-09-05 21:44:31 +0100

    update category and sort

--- src/services/phones.services.ts
+++ src/services/phones.services.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { prisma } from "../database/prisma";
 import { TPhone, TPhoneCreate, TPhoneUpdate } from "../schemas/phones.schema";
 
@@ -10,16 +11,38 @@ export class PhoneServices {
     const data = await prisma.product.findUnique({ where: { id } });
     return data;
   }
-  async findMany() {
-    const data = await prisma.product.findMany();
-    return data;
-  }
-  async findManyByCategory(category: string) {
+  async findMany({
+    category,
+    sort,
+  }: { category?: string; sort?: string } = {}) {
+    let orderBy;
+
+    switch (sort) {
+      case "priceAsc":
+        orderBy = { price: Prisma.SortOrder.asc };
+        break;
+      case "priceDesc":
+        orderBy = { price: Prisma.SortOrder.desc };
+        break;
+      case "releaseDateAsc":
+        orderBy = { createdAt: Prisma.SortOrder.asc };
+        break;
+      case "releaseDateDesc":
+        orderBy = { createdAt: Prisma.SortOrder.desc };
+        break;
+      default:
+        orderBy = { createdAt: Prisma.SortOrder.desc };
+    }
+
     const data = await prisma.product.findMany({
-      where: { category: category },
+      where: {
+        category: category ? { equals: category } : undefined,
+      },
+      orderBy,
     });
     return data;
   }
+
   async update(id: number, body: TPhoneUpdate) {
     const data = await prisma.product.update({ where: { id }, data: body });
     return data;
